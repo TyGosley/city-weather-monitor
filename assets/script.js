5ddba0c4812fca18c4d6626ea2690ebc45add848
TyGosley <tygosley@gmail.com>
2023-02-06 17:38:59 -0800

    created variables in js, psuedo code in js (starting over), and cleaned up CSS

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// declare variables
+
+const APIKey = "9e38d5126129d897b3ddb585abf79cdc";
+const currentDay = dayjs().format("MM DD, YYYY");
+const display = document.querySelector("#display");
+const currentTemp = document.querySelector("#temp");
+const currentWind = document.querySelector("#wind");
+const currentHumidity = document.querySelector("#humidity");
+const citiesEl = document.querySelector("#cities");
+const searchBtn = document.querySelector("#searchBtn");
+const forecastResultsEl = document.querySelector("#forecastResults");
+
+
+// TODO: Create function to search for city
+
+// TODO: create search history buttons *Local Storage*
+
+// TODO: Add delete buttons on history searches
+
+// TODO: Locate city coordinates
+
+// TODO: Display date(dayjs), city, temp, wind, humidity in containerMain card
+
+// TODO:  Add icons to cards displaying weather conditions
+
+// TODO: display in imperial units
+
+// TODO: Display 5 seperate cards displaying attributes in container5Day
+ 
+// TODO: Add Event Listener
+// searchBtn.addEvenetListener("click", getWeather);
+
